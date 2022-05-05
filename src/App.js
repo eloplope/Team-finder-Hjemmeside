33800fac288f8120f3a39ee75bfab7943d5b5f1b
eloplope <gitlingen@gmail.com>
2022-05-05 12:27:11 +0200

    find side med david skitseret

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import logo3 from './controoler.png';
 import logo4 from './profilimg.png';
 import maximg from './maximg.jpg';
 import maxto from './maxto.jpg';
+import davidet from './davidet.jpg';
+import davidto from './Davidto.jpg';
 import billedtre from './billedtre.png';
 import jatak from './jatak.png';
 import nejtak from './nejtak.png';
@@ -165,17 +167,36 @@ function Chat() {
 function PageThree() {
   return (
     <>
-      <h1>Finder</h1>
-      <div className=" text-center">
-        <img src={billedtre} className="img-fluid" style={{ width: 300 }} />
-      </div>
+      <br></br>
       <div className="row text-center">
         <div className="col">
-          <img src={jatak} className="img-fluid" style={{ width: 200 }} />
+          <img src={davidto} className="img-fluid" style={{ width: 200 }} />
         </div>
         <div className="col">
+          <img src={davidet} className="img-fluid" style={{ width: 250 }} />
+        </div>
+        <div className="col">
+          <img src={billedtre} className="img-fluid" style={{ width: 200 }} />
+        </div>
+      </div>
+      <div className='text-center'>
+        <h1>David Ulrich Simonsen 18</h1>
+      </div>
+      <div className="row text-center">
+        <div className="col text-right">
+          <br></br>
+        </div>
+        <div className="col text-right">
+          <img src={jatak} className="img-fluid" style={{ width: 200 }} />
+        </div>
+        <div className="col text-center">
+          <p>Hej mit navn er David. Jeg elsker Counter strike, og animee. Jeg er også meget glad for baguette og humus.</p>
+        </div>
+        <div className="col text-left">
           <img src={nejtak} className="img-fluid" style={{ width: 200 }} />
-
+        </div>
+        <div className="col text-right">
+          <br></br>
         </div>
       </div>
     </>
