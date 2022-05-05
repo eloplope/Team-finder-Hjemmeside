c3e608cf0c6871a0e323d33112de746228f31d93
eloplope <gitlingen@gmail.com>
2022-05-05 10:45:06 +0200

    mere find side

--- src/App.js
+++ src/App.js
@@ -2,6 +2,11 @@ import logo from './logo.svg';
 import logo2 from './chaat.png';
 import logo3 from './controoler.png';
 import logo4 from './profilimg.png';
+import maximg from './maximg.jpg';
+import maxto from './maxto.jpg';
+import billedtre from './billedtre.png';
+import jatak from './jatak.png';
+import nejtak from './nejtak.png';
 import logoteam from './teamlogo.png';
 import React from 'react';
 import {
@@ -159,7 +164,21 @@ function Chat() {
 
 function PageThree() {
   return (
-    <h1>Finder</h1>
+    <>
+      <h1>Finder</h1>
+      <div className=" text-center">
+        <img src={billedtre} className="img-fluid" style={{ width: 300 }} />
+      </div>
+      <div className="row text-center">
+        <div className="col">
+          <img src={jatak} className="img-fluid" style={{ width: 200 }} />
+        </div>
+        <div className="col">
+          <img src={nejtak} className="img-fluid" style={{ width: 200 }} />
+
+        </div>
+      </div>
+    </>
   );
 
 }
@@ -167,38 +186,57 @@ function PageThree() {
 function Profil() {
   return (
     <>
-      <br></br>
-      <h2>Rediger dine profil oplysninger</h2>
-      <div style={{ maxWidth: 400 }}>
-        <div className="mb-3">
-          <label className="form-label">Navn:</label>
-          <input type="name" className="form-control" id="exampleFormControlInput1" placeholder=" " />
-        </div>
-        <div>
-          <label className="form-label">Telefonnummer:</label>
-        </div>
-        <div className="input-group flex-nowrap">
-          <span className="input-group-text" id="addon-wrapping">45+</span>
-          <input type="text" className="form-control" placeholder="nummer" aria-label="phonenumber" aria-describedby="addon-wrapping" />
+      <div className="row text-left">
+        <br></br>
+        <h2>Rediger dine profil oplysninger</h2>
+        <div className="col">
+          <div style={{ maxWidth: 400 }}>
+            <div className="mb-3">
+              <label className="form-label">Navn:</label>
+              <input type="name" className="form-control" id="exampleFormControlInput1" placeholder=" " />
+            </div>
+            <div>
+              <label className="form-label">Telefonnummer:</label>
+            </div>
+            <div className="input-group flex-nowrap">
+              <span className="input-group-text" id="addon-wrapping">45+</span>
+              <input type="text" className="form-control" placeholder="nummer" aria-label="phonenumber" aria-describedby="addon-wrapping" />
 
-        </div>
-        <div className="mb-3">
-          <label className="form-label"><br></br>Alder:</label>
-          <SliderWithInputFormControl></SliderWithInputFormControl>
-        </div>
+            </div>
+            <div className="mb-3">
+              <label className="form-label"><br></br>Alder:</label>
+              <SliderWithInputFormControl></SliderWithInputFormControl>
+            </div>
 
-        <div className="mb-3">
-          <label className="form-label">Email addresse:</label>
-          <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com" />
+            <div className="mb-3">
+              <label className="form-label">Email addresse:</label>
+              <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com" />
+            </div>
+            <div className="mb-3">
+              <label className="form-label">Din biografi:</label>
+              <textarea className="form-control" id="exampleFormControlTextarea1" placeholder="Hej, mit navn er joe, jeg elsker spil." rows="3"></textarea>
+            </div>
+            <div className="d-grid gap-2">
+              <button className="btn btn-primary" type="button">Gem ændringer</button>
+            </div>
+          </div>
         </div>
-        <div className="mb-3">
-          <label className="form-label">Din biografi:</label>
-          <textarea className="form-control" id="exampleFormControlTextarea1" placeholder="Hej, mit navn er joe, jeg elsker spil." rows="3"></textarea>
+        <div className="col">
+          <div className="row align-items-end">
+            <div className="col ">
+              <img src={maximg} className="img-fluid" style={{ width: 200 }} />
+            </div>
+            <div className="col ">
+              <img src={maxto} className="img-fluid" style={{ width: 200 }} />
+            </div>
+            <div className="col ">
+              <img src={billedtre} className="img-fluid" style={{ width: 200 }} />
+            </div>
+          </div>
+          <div className="d-grid">
+            <button className="btn btn-primary" type="button">Skift billeder</button>
+          </div>
         </div>
-        <div className="d-grid gap-2">
-          <button className="btn btn-primary" type="button">Gem ændringer</button>
-        </div>
-
       </div>
     </>
 
