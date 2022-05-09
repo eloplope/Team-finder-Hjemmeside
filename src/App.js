64a034cf017067361a91086be777add770817289
petey33 <psterner@gmail.com>
2022-05-09 08:53:23 +0200

    Oprydning...

--- src/App.js
+++ src/App.js
@@ -1,10 +1,4 @@
 
-import davidet from './davidet.jpg';
-import davidto from './Davidto.jpg';
-import davidtre from './Davidtre.png';
-
-import jatak from './jatak.png';
-import nejtak from './nejtak.png';
 import React, { useContext, useState, useEffect } from 'react';
 import {
   Route,
@@ -22,6 +16,7 @@ import Chat from './Chat';
 import Layout from './Layout';
 import Logud from './Logud';
 import Profil from './Profil';
+import PageThree from './PageThree';
 import './App.css';
 import './styles.css';
 import { AuthContext } from "./Context.js";
@@ -49,10 +44,11 @@ function AuthProvider(inner) {
   let [user, setUser] = useState();
 
   useEffect(() => {
-    console.log("Opsætter firebase...", user);
+    console.log("Opsætter firebase...");
     const unsubscribe = authProvider.firebaseSetup(user, setUser);
     return (()=>{
       unsubscribe();
+      console.log("Afsluttet brug af firebase...");
     });
   }, []);
 
@@ -82,43 +78,6 @@ function App() {
   );
 }
 
-function PageThree() {
-  return (
-    <>
-      <br></br>
-      <div className="row text-center">
-        <div className="col">
-          <img src={davidto} className="img-fluid" style={{ width: 200 }} alt="" />
-        </div>
-        <div className="col">
-          <img src={davidet} className="img-fluid" style={{ width: 250 }} alt="" />
-        </div>
-        <div className="col">
-          <img src={davidtre} className="img-fluid" style={{ width: 200 }} alt="" />
-        </div>
-      </div>
-      <div className='text-center'>
-        <h1>David Ulrich Simonsen 18</h1>
-      </div>
-      <div className="row text-center">
-        <div className="col text-right">
-          <br></br>
-        </div>
-        <div className="col text-right">
-          <img src={jatak} className="img-fluid" style={{ width: 200 }} alt="" />
-        </div>
-        <div className="col text-center">
-          <p>Hej mit navn er David. Jeg elsker Counter strike, og animee. Jeg er også meget glad for baguette og humus.</p>
-        </div>
-        <div className="col text-left">
-          <img src={nejtak} className="img-fluid" style={{ width: 200 }} alt="" />
-        </div>
-        <div className="col text-right">
-          <br></br>
-        </div>
-      </div>
-    </>
-  );
-}
+
 
 export default App;
